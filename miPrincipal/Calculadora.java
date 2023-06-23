d9cb83fad9df173f810acf6158b3c560c1f736b9
Nestor-Leyva <75275108+Nestor-Leyva@users.noreply.github.com>
2023-06-23 18:11:26 +0000

    Lista la calculadora para la tarea de los alumnos

--- miPrincipal/Calculadora.java
+++ miPrincipal/Calculadora.java
@@ -5,16 +5,5 @@ public class Calculadora {
     public int suma(int x, int y) {
         return x + y;
     }
-    
-    public int resta(int x, int y) {
-        return x-y;
-    }
 
-    public int multiplica(int x, int y) {
-        return x * y;
-    }
-
-    public double divide(int x, int y) {
-        return (double) x / y;
-    }
 }
