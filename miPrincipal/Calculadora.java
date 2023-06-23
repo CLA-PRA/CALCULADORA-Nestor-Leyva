f730bf82f9cc20de2e49b53ec138b83c3c56f065
Nestor-Leyva <75275108+Nestor-Leyva@users.noreply.github.com>
2023-06-23 10:53:44 -0700

    Initial commit

---
+++ miPrincipal/Calculadora.java
@@ -0,0 +1,20 @@
+package miPrincipal;
+
+public class Calculadora {
+
+    public int suma(int x, int y) {
+        return x + y;
+    }
+    
+    public int resta(int x, int y) {
+        return x-y;
+    }
+
+    public int multiplica(int x, int y) {
+        return x * y;
+    }
+
+    public double divide(int x, int y) {
+        return (double) x / y;
+    }
+}
