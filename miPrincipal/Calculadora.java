09f77c07418c8a43be7098b349e5d743a76b139f
Nestor-Leyva <75275108+Nestor-Leyva@users.noreply.github.com>
2023-06-23 18:12:47 +0000

    Lista con return 0

--- miPrincipal/Calculadora.java
+++ miPrincipal/Calculadora.java
@@ -3,7 +3,18 @@ package miPrincipal;
 public class Calculadora {
 
     public int suma(int x, int y) {
-        return x + y;
+        return 0;
+    }
+    
+    public int resta(int x, int y) {
+        return 0;
     }
 
+    public int multiplica(int x, int y) {
+        return 0;
+    }
+
+    public double divide(int x, int y) {
+        return (double) 0;
+    }
 }
